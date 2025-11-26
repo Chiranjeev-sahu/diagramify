a6733fa40cb44d4fba7c812fb1242f95838f9922
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-11-26 22:32:39 +0530

    trust proxy for rate limiting behind reverse proxy

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import userRouter from "./src/routes/user.routes.js";
 import { generateDiagramsPublic } from "./src/controllers/diagram.controller.js";
 
 const app = express();
+app.set("trust proxy",1);
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cors(corsOptions));
