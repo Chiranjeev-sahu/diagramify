b40feff82b9d1eef580449b5f30bab02dd862175
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2025-11-30 16:00:00 +0530

    refactor(frontend): Cleanup unused hooks and stores

--- frontend/src/store/useAppStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useAppStore = create(set => ({
-  trials: 0,
-  incrementTrials: () => set(s => ({ trials: s.trials + 1 })),
-  resetTrials: () => set({ trials: 0 }),
-}));
