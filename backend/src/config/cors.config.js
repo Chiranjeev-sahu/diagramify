36b6707d850a72d3d69a3f7099f3d07e25a93013
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2026-03-25 23:30:28 +0530

    changed cors origin

--- backend/src/config/cors.config.js
+++ backend/src/config/cors.config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://diagramify-theta.vercel.app"],
+  origin: ["http://localhost:5173", "https://diagramify-theta-v2.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 
